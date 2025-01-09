b62d87f6da10d8646fddf8826ec1de5b5dfb66f1
marcello <mvlservicosinfo@gmail.com>
2025-01-09 07:34:21 -0300

    "Arquivo de Servico"

--- src/main/java/servico/Servico.java
+++ src/main/java/servico/Servico.java
@@ -1,17 +1,17 @@
 package servico;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Servico {
 
+    @Bean
     public <T extends Comparable<T>> T max(T x, T y) {
         return x.compareTo(y) > 0 ? x : y;
     }
-
+    @Bean
     public <T extends Comparable<T>> T min(T x, T y) {
-        return x.compareTo(y) > 0 ? x : y;
+        return x.compareTo(y) < 0 ? x : y;
     }
-
-
 }
