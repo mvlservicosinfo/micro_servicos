4f9f4a20ca31882d1a06ea469eae30f8091e6dec
marcello <mvlservicosinfo@gmail.com>
2025-01-09 14:41:20 -0300

    "Aplicação"

--- src/main/java/br/microservicos/microservico/MicroServicoApplication.java
+++ src/main/java/br/microservicos/microservico/MicroServicoApplication.java
@@ -1,13 +1,16 @@
 package br.microservicos.microservico;
 
+import controller.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import servico.Servico;
 
 @SpringBootApplication
 public class MicroServicoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServicoApplication.class, args);
+		Controller controller = new Controller();
 	}
 
 }
